789b50d79dab9ae8fa4963afa2e2e9b18d87ddf2
Prem <pkthogit@users.noreply.github.com>
2025-01-29 16:28:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import './App.css'; // Import global styles
+
+const App = () => {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        {/* Header */}
+        <Header />
+
+        {/* Main Content */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </main>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
